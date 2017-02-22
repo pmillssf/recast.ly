def70a2ea80f8ad4896bf05db55b67637bc73d9b
Hack Reactor Students <accounts@hackreactor.com>
2017-02-22 15:16:01 -0800

    Move App render code to index.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,5 +13,3 @@ var App = () => (
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
-
-ReactDOM.render(<App />, document.getElementById('app'));
